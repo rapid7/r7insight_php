3c40a90907c6ff6a61aed8f82e159d1cc8819d7b
Szymon Bialkowski <szymon_bialkowski@rapid7.com>
2020-07-09 11:54:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# r7insight_php
+
+With these simple steps you can send your PHP application logs to the Insight Platform.
+
+- [r7insight_php](#r7insight_php)
+	- [Insight Platform Setup](#insight-platform-setup)
+	- [Parameter Setup](#parameter-setup)
+	- [Adding a Custom Host Name and Host ID sent in your PHP log events](#adding-a-custom-host-name-and-host-id-sent-in-your-php-log-events)
+	- [Code Setup](#code-setup)
+
+## Insight Platform Setup
+
+Once you have made your account on the Insight platform, log in and use the "Quick Add" option in the "Add data" page to add a new log, using the Token TCP option.
+
+The Log Token and data endpoint will be displayed.  
+The Log Token is a unique identifier for the log allowing for write-only access.
+Take note of these.
+
+
+## Parameter Setup
+Inside the `r7insight_php` folder, open `r7insight.php` and fill in the `LOG_TOKEN` parameter.
+
+`LOG_TOKEN` is the token you copied earlier from the Insight Platform UI, and associates that logger with the log in Insight Platform.
+
+In the `REGION` field, enter the region code - e.g. `us`, `eu`, `ca`, `au` etc.
+
+You can also supply both of these by the use of environment variables with the same name -- `LOG_TOKEN` and `REGION`.
+
+
+Adding a Custom Host Name and Host ID sent in your PHP log events
+---------------
+To set a custom Host Name that will appear in your PHP log events as Key / Value pairs:
+
+Inside the `r7insight_php` folder, open `r7insight.php` and fill in the parameters as follows:
+
+	$HOST_NAME_ENABLED = true;
+
+	$HOST_NAME = "Custom_host_name_here";
+
+	$HOST_ID = "Custom_ID_here_12345";
+
+The `$HOST_NAME` constant can be left as an empty string, and the library will automatically attempt to assign a host name from 
+your local host machine and use that as the custom host name.
+
+To set a custom Host ID that will appear in your PHP log events as Key / Value pairs:
+Enter a value instead of the empty string in `$HOST_ID = "";`  
+If no `$HOST_ID` is set and the empty string is left unaltered, no Host ID or Key / Value pairing will appear in your PHP logs.
+
+If you want to send log events in JSON format, then set the `$USE_JSON` field to `true`. If you set it to `false`, then the logs will be sent in KVP (Key Value Pair) format.
+
+## Code Setup
+
+Now you need to download/clone the library from GitHub and place the folder in your apps directory.
+
+To use it in your code, enter the following lines, making changes accordingly if you place it in a different location.
+```php
+require dirname(__FILE__) . './r7insight_php/r7insight.php';
+
+// The following levels are available
+$log->debug("Isn't that the fault of the voters?");
+$log->info("That's because sometimes I go by my maiden name.");
+$log->notice("Give me the strongest thing you got.");
+$log->crit("Awfully big moustache.");
+$log->error("Yeah, and when I find the guy that did it...");
+$log->alert("That's ok. I sometimes go by my maiden name.");
+$log->emerg("Every time I order out.");
+```
---
+++ unit_tests/R7LoggerTests.php
@@ -0,0 +1,109 @@
+<?php
+	use PHPUnit\Framework\TestCase;
+
+	class R7LoggerTest extends TestCase
+	{
+		const token = '00112233-4455-6677-8899-aabbccddeeff';
+		const region = 'eu';
+		const persistent = true;
+		const ssl = true;
+		const severity = LOG_DEBUG;
+		const datahubEnabled = false;
+		const datahubIPAddress = '';
+		const datahubPort = 10000;
+		const host_id = '123-123';
+		const host_name = 'car';
+		const host_name_enabled = true;
+		const add_local_timestamp = true;
+		const use_json = true;
+
+		public function testMultiplyConnections()
+		{
+			$logFirst = R7Logger::getLogger('00112233-4455-6677-8899-aabbccddeef0', self::region, self::persistent, self::ssl, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+			$logSecond = R7Logger::getLogger('00112233-4455-6677-8899-aabbccddeef1', self::region, self::persistent, self::ssl, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+			$logThird = R7Logger::getLogger('00112233-4455-6677-8899-aabbccddeef2', self::region, self::persistent, self::ssl, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertNotEquals('00112233-4455-6677-8899-aabbccddeef0', $logSecond->getToken());
+			self::assertNotEquals('00112233-4455-6677-8899-aabbccddeef1', $logThird->getToken());
+
+			self::assertEquals('00112233-4455-6677-8899-aabbccddeef0', $logFirst->getToken());
+			self::assertEquals('00112233-4455-6677-8899-aabbccddeef1', $logSecond->getToken());
+			self::assertEquals('00112233-4455-6677-8899-aabbccddeef2', $logThird->getToken());
+		}
+
+		public function testIsPersistent()
+		{
+			$log = R7Logger::getLogger(self::token, self::region, false, self::ssl, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertFalse($log->isPersistent());
+
+			self::tearDown();
+
+			$log = R7Logger::getLogger(self::token, self::region, self::persistent, self::ssl, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertTrue($log->isPersistent());
+		}
+
+		public function testIsTLS()
+		{
+			$log = R7Logger::getLogger(self::token, self::region, self::persistent, false, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertFalse($log->isTLS());
+
+			self::tearDown();
+
+			$log = R7Logger::getLogger(self::token, self::region, self::persistent, self::ssl, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertTrue($log->isTLS());
+		}
+
+		public function testGetPort()
+		{
+			$log = R7Logger::getLogger(self::token, self::region, self::persistent, false, self::severity, self::datahubEnabled, self::datahubIPAddress, 20000, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+
+			self::assertEquals(10000, $log->getPort());
+
+			self::tearDown();
+
+			$log = R7Logger::getLogger(self::token, self::region, self::persistent, true, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertEquals(20000, $log->getPort());
+		}
+
+		public function testGetAddress()
+		{
+			$log = R7Logger::getLogger(self::token, self::region, self::persistent, false, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertEquals('tcp://eu.data.logs.insight.rapid7.com', $log->getAddress());
+
+			self::tearDown();
+			$log = R7Logger::getLogger(self::token, self::region, self::persistent, true, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+
+			self::assertEquals('tls://eu.data.logs.insight.rapid7.com', $log->getAddress());
+		}
+
+		public function testEmptyToken() {
+			$this->expectException(InvalidArgumentException::class);
+
+			R7Logger::getLogger($this->definitely_a_valid_attribute, self::region, self::persistent, true, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+		}
+
+		public function testInvalidTokenFormat() {
+			$this->expectException(InvalidArgumentException::class);
+
+			R7Logger::getLogger("I'm a UUID I swear...", self::region, self::persistent, true, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+		}
+
+		public function testEmptyStringToken() {
+			$this->expectException(InvalidArgumentException::class);
+
+			R7Logger::getLogger("", self::region, self::persistent, true, self::severity, self::datahubEnabled, self::datahubIPAddress, self::datahubPort, self::host_id, self::host_name, self::host_name_enabled, self::add_local_timestamp, self::use_json);
+		}
+
+		public function tearDown(): void
+		{
+			R7Logger::tearDown();
+		}
+	}	
+?>
